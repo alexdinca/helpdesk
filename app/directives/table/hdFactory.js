609d6975f2d800c10c5915d8b154914391f890b2
dnk <dnkalex@gmail.com>
2016-01-19 21:40:10 +0200

    fix for hdTable directive

--- app/directives/table/hdFactory.js
+++ app/directives/table/hdFactory.js
@@ -5,18 +5,23 @@ app
     .factory('hdFactory',
 
         function ($http, $rootScope) {
-            var factory = {
-            	url: '',
-            	items: ''
+            var instance = function(){
+                var factory = {
+                    url: '',
+                    items: ''                    
+                };           	
+            
+                factory.fetchData = function(take, order, sort, page){
+                    return $http.get(factory.url + factory.items + "/list.php?take="+take+"&order="+order+"&sort="+sort+"&page="+page);
+                };
+
+                factory.countData = function(){
+                    return $http.get(factory.url + factory.items + "/list.php?count");
+                };
+
+                return factory;
             };                       
 
-            factory.fetchData = function(take, order, sort, page){
-                return $http.get(factory.url + factory.items + "/list.php?take="+take+"&order="+order+"&sort="+sort+"&page="+page);
-            };
-
-            factory.countData = function(){
-                return $http.get(factory.url + factory.items + "/list.php?count");
-            };
-
-            return factory;
+            return instance;
+            
         });
